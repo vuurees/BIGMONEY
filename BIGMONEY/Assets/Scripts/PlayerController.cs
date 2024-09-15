a178668880350c18a5559628d009b11c64187a84
vuurees <andrewlensch19@gmail.com>
2024-09-15 09:34:52 -0700

    new player controller

--- BIGMONEY/Assets/Scripts/PlayerController.cs
+++ BIGMONEY/Assets/Scripts/PlayerController.cs
@@ -1,47 +1,37 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    
+    private Rigidbody myRB;
+    Camera PlayerCam;
 
     Vector2 camRotation;
 
+    public bool sprintMode = false;
 
-    //Movement
-    Rigidbody myRB;
+    private bool isGrounded;
+    public int doubleJump = 0;
+    [Header("Movement Settings")]
     public float speed = 10.0f;
-    public float jumpHeight = 5.0f;
-    public float grounddetectdistance = 1;
+    public float sprintMultiplier = 2.5f;
+    public float jumpHeight = 2.0f;
+    public float groundDetectDistance = 1.5f;
 
-     //Sprint
-     public float sprintMulitplyer = 2.5f;
-     public Boolean sprintMode = false;
-     public Boolean ToggleSprint = true;
 
-     //Crouch
-     public Boolean isCrouching = false;
-
-     //Prone
-     public Boolean isProne = false;
-
-    //Camera
-    Camera playerCam;
+    [Header("User Settings")]
+    public bool sprintToggleOption = false;
     public float mouseSensitivity = 2.0f;
-    public float Xsensitivity = 2f;
-    public float Ysensitivity = 2f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
     public float camRotationLimit = 90f;
-    public float fieldofview = 90f;
-
-
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
-        playerCam = transform.GetChild(0).GetComponent<Camera>();
+        PlayerCam = transform.GetChild(0).GetComponent<Camera>();
 
         camRotation = Vector2.zero;
         Cursor.visible = false;
@@ -51,36 +41,55 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+        isGrounded = Physics.Raycast(transform.position, -transform.up, groundDetectDistance);
         camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
         camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
-     
         camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
 
-
-        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        PlayerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
         transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
 
-        
         Vector3 temp = myRB.velocity;
 
-        temp.x = Input.GetAxisRaw("Horizontal") * speed;
-        temp.z = Input.GetAxisRaw("Vertical") * speed;
+        if (!sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+                sprintMode = true;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            sprintMode = true;
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-            sprintMode = false;
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+                sprintMode = false;
+        }
+
+        if (sprintToggleOption)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetAxisRaw("Vertical") > 0)
+                sprintMode = true;
+
+            if (Input.GetAxisRaw("Vertical") <= 0)
+                sprintMode = false;
+        }
 
 
-        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, grounddetectdistance))
+
+        if (!sprintMode)
+            temp.x = Input.GetAxisRaw("Vertical") * speed;
+
+        if (sprintMode)
+            temp.x = Input.GetAxisRaw("Vertical") * speed * sprintMultiplier;
+
+        temp.z = Input.GetAxisRaw("Horizontal") * speed;
+
+
+        if (isGrounded)
+            doubleJump = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || doubleJump < 1))
+        {
             temp.y = jumpHeight;
+            doubleJump += 1;
+        }
 
-      
-        myRB.velocity = (transform.forward * temp.z) + (transform.right * temp.x) + (transform.up * temp.y);
-
-
+        myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
     }
-
 }
